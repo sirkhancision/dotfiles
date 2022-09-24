9870ea5af49d22322a64bbd1b82cb1dc767cf7ab
Jorge Santiago <jsantiago12tone@gmail.com>
2022-09-24 20:10:49 -0300

    Updated waybar and sway's configs, along with sway's scripts, and also put them into sway's directory for better encapsulation and nativeness. Also separated qt5ct and qt6ct colorschemes to their own directories.

--- sway/scripts/screenclip.sh
+++ sway/scripts/screenclip.sh
@@ -2,7 +2,7 @@
 
 pic_dir=$(xdg-user-dir PICTURES)
 
-slurp | grim -g - $pic_dir/Screenshots/scrn-$(date +"%d-%m-%Y-%H-%M-%S").png
-paplay $HOME/.config/sway/audio/screen-capture.ogg
+slurp | grim -g - $pic_dir/Screenshots/scrn-$(date +"%d-%m-%Y-%H-%M-%S").png \
+    && paplay $HOME/.config/sway/audio/screen-capture.ogg
 image_path=$(echo -n "$pic_dir/Screenshots/" && ls -Art $pic_dir/Screenshots/ | tail -n 1)
 wl-copy < $image_path
