12db3d72d2bb5920a15c3df0ecdee58dbd8da079
Jorge Santiago <jsantiago12tone@gmail.com>
2022-09-28 00:40:25 -0300

    Added emoji font to fonts.conf, removed the notification sound used in volume changing scripts for sway, updated sway and waybar's config.

--- sway/scripts/volume-decrease.sh
+++
@@ -1,4 +0,0 @@
-##!/bin/zsh
-
-pactl set-sink-volume @DEFAULT_SINK@ -5%
-paplay $HOME/.config/sway/audio/notification.ogg
--- sway/scripts/volume-mute.sh
+++
@@ -1,4 +0,0 @@
-##!/bin/zsh
-
-pactl set-sink-mute @DEFAULT_SINK@ toggle
-paplay $HOME/.config/sway/audio/notification.ogg
