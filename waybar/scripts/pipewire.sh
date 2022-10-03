c929d5bcc6973a156cbd99085a7d834e939feccf
Jorge Santiago <jsantiago12tone@gmail.com>
2022-10-03 17:26:55 -0300

    Corrected formatting for the backlight module in Waybar, and made the Pipewire module posix compliant along with simplifying its logic (thanks zixx999 and iritegood on reddit).

--- waybar/scripts/pipewire.sh
+++ waybar/scripts/pipewire.sh
@@ -1,15 +1,15 @@
 ##!/bin/zsh
-
+
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | \
     sed 's/Volume: //' | \
-    xargs -I {} zsh -c 'qalc -t -s "decimal comma off" "{} * 100"')
+    xargs -I {} $SHELL -c 'printf "%0.0f\n" "{} * 100"')
 
-if [ $(echo $volume | grep "MUTED") -eq "" ] || [ $(echo $volume) -ne 0 ]; then 
-    if [[ $volume -le 100 && $volume -gt 50 ]]; then
+if [ ! case $volume in *MUTED* ] || [ $volume -ne 0 ]; then 
+    if [[ $volume -gt 50 ]]; then
         echo " $volume%"
-    elif [[ $volume -le 50 && $volume -gt 25 ]]; then
+    elif [[ $volume -gt 25 ]]; then
         echo " $volume%"
-    elif [[ $volume -le 25 && $volume -gt 0 ]]; then
+    elif [[ $volume -gt 0 ]]; then
         echo " $volume%"
     fi
 else
