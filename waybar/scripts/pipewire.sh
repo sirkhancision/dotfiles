8bd19a42a3f0c2950862fbf07b22ba9bdbb12326
Jorge Santiago <jsantiago12tone@gmail.com>
2022-10-29 02:11:53 -0300

    Changed waybar/scripts/pipewire.sh shell to bash

--- waybar/scripts/pipewire.sh
+++ waybar/scripts/pipewire.sh
@@ -1,28 +1,41 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
-zmodload zsh/regex
 set -e
+
+# https://blog.dhampir.no/content/sleeping-without-a-subprocess-in-bash-and-how-to-sleep-forever
+snore() {
+	local IFS
+	[[ -n "${_snore_fd:-}" ]] || exec {_snore_fd}<> <(:)
+	read -r ${1:+-t "$1"} -u $_snore_fd || :
+}
 
 DELAY=0.2
 
-while sleep $DELAY; do
-    wp_output=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-    if [[ $wp_output =~ ^Volume:[[:blank:]]([0-9]+)\.([0-9]{2})([[:blank:]].MUTED.)?$ ]]; then
-        if [[ -n ${match[3]} ]]; then
-            printf "MUTE\n"
-        else
-            volume=$(( ${match[1]}${match[2]} ))
-            if [[ $volume -gt 50 ]]; then
-                printf " $volume%%\n"
-            elif [[ $volume -gt 25 ]]; then
-                printf " $volume%%\n"
-            elif [[ $volume -gt 0 ]]; then
-                printf " $volume%%\n"
-            else
-                printf "---\n"
-            fi
-        fi
-    fi
+while snore $DELAY; do
+	WP_OUTPUT=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+
+	if [[ $WP_OUTPUT =~ ^Volume:[[:blank:]]([0-9]+)\.([0-9]{2})([[:blank:]].MUTED.)?$ ]]; then
+		if [[ -n ${BASH_REMATCH[3]} ]]; then
+			printf "MUTE\n"
+		else
+			VOLUME=$((10#${BASH_REMATCH[1]}${BASH_REMATCH[2]}))
+			ICON=(
+				""
+				""
+				""
+			)
+
+			if [[ $VOLUME -gt 50 ]]; then
+				printf "%s" "${ICON[0]} "
+			elif [[ $VOLUME -gt 25 ]]; then
+				printf "%s" "${ICON[1]} "
+			elif [[ $VOLUME -ge 0 ]]; then
+				printf "%s" "${ICON[2]} "
+			fi
+
+			printf "$VOLUME%%\n"
+		fi
+	fi
 done
 
 exit 0
