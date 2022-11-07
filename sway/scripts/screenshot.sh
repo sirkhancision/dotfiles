9de3a7c65b1fd36faa0009c49ea23b2c718fc54c
Jorge Santiago <jsantiago12tone@gmail.com>
2022-11-07 00:43:21 -0300

    Update Sway and Waybar stuff

--- sway/scripts/screenshot.sh
+++ sway/scripts/screenshot.sh
@@ -8,7 +8,7 @@ SCREENSHOTS_DIR=$PIC_DIR/Screenshots
 
 grim "$SCREENSHOTS_DIR"/"$IMAGE_NAME" &&
 	paplay "$HOME"/.config/sway/audio/screen-capture.ogg
-wl-copy <"$SCREENSHOTS_DIR"/"$IMAGE_NAME"
+wl-copy -t image/png <"$SCREENSHOTS_DIR"/"$IMAGE_NAME"
 
 NOTIFICATION_TEXT="<i>$IMAGE_NAME</i>\nCopiado para a área de transferência."
 dunstify 'Captura de tela' "$NOTIFICATION_TEXT" -I "$SCREENSHOTS_DIR"/"$IMAGE_NAME"
--- sway/scripts/windowshot.sh
+++ sway/scripts/windowshot.sh
@@ -10,7 +10,7 @@ swaymsg -t get_tree |
 	jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' |
 	slurp | grim -g - "$SCREENSHOTS_DIR"/"$IMAGE_NAME" &&
 	paplay "$HOME"/.config/sway/audio/screen-capture.ogg
-wl-copy <"$SCREENSHOTS_DIR"/"$IMAGE_NAME"
+wl-copy -t image/png <"$SCREENSHOTS_DIR"/"$IMAGE_NAME"
 
 NOTIFICATION_TEXT="<i>$IMAGE_NAME</i>\nCopiado para a área de transferência."
 dunstify 'Captura de tela' "$NOTIFICATION_TEXT" -I "$SCREENSHOTS_DIR"/"$IMAGE_NAME"
