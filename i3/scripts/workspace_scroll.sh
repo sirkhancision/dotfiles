ce4f4f63a63bebc634748e29d84ca7eb8f65efb4
Jorge Santiago <jsantiago12tone@gmail.com>
2022-12-03 16:50:30 -0300

    Change formatting of workspace_scroll.sh

--- i3/scripts/workspace_scroll.sh
+++ i3/scripts/workspace_scroll.sh
@@ -1,25 +1,22 @@
-#!/bin/bash
+#!/bin/sh
+# Switch to the next non-empty workspace in i3wm
 
-# switch to the next non-empty workspace in i3wm
-# (i.e. the next workspace that has at least one window in it).
-# This script is intended to be bound to a key in i3's config file.
-
-# Get the names of all workspaces.
+# Get the names of all workspaces
 WORKSPACES=$(i3-msg -t get_workspaces | jq -r '.[].name')
 
-# Get the name of the currently focused workspace.
-current=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
+# Get the name of the currently focused workspace
+CURRENT=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
 
-# Get the name of the next non-empty workspace.
-next=$(echo "$WORKSPACES" | grep -A1 "$current" | tail -n1)
+# Get the name of the next non-empty workspace
+NEXT=$(echo "$WORKSPACES" | grep -A1 "$CURRENT" | tail -n1)
 
 # Get the name of the last workspace
-last=$(echo "$WORKSPACES" | tail -n1)
+LAST=$(echo "$WORKSPACES" | tail -n1)
 
-# If there is no next workspace, wrap around to the first one.
-if [ "$current" -eq "$last" ]; then
-	next=$(echo "$WORKSPACES" | head -n1)
+# If there is no next workspace, wrap around to the first one
+if [ "$CURRENT" -eq "$LAST" ]; then
+	NEXT=$(echo "$WORKSPACES" | head -n1)
 fi
 
-# Switch to the next non-empty workspace.
-i3-msg workspace "$next"
+# Switch to the next non-empty workspace
+i3-msg workspace "$NEXT"
