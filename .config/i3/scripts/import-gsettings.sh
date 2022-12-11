240805e5914c31b743e9abedf66537a2012cc9cb
Jorge Santiago <jsantiago12tone@gmail.com>
2022-12-11 17:42:19 -0300

    Correct paths at i3's config and import-gsettings

--- .config/i3/scripts/import-gsettings.sh
+++ .config/i3/scripts/import-gsettings.sh
@@ -3,9 +3,9 @@
 set -e
 
 # usage: import-gsettings
-config="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
+config="$HOME/.config/gtk-3.0/settings.ini"
 if [ ! -f "$config" ]; then
-	exit 1
+    exit 1
 fi
 
 gnome_schema="org.gnome.desktop.interface"
