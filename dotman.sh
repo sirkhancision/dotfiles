18f5a92f10903d919c622e4582a1947a137dd894
Jorge Santiago <jsantiago12tone@gmail.com>
2022-12-24 22:28:38 -0300

    Add SIGINT handling, start using fd instead of find, and simplify the command for finding files in dotman.sh

--- dotman.sh
+++ dotman.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # dotman - Dotfiles Manager
-# Written in POSIX shell
+# Written for POSIX shell
 # by sirkhancision
+
+# end successfully if, for example, the user presses Ctrl+C
+# to exit the script
+end_script() {
+    printf "\n"
+    exit 0
+}
+
+trap 'end_script' INT
 
 # adjust according to the directory of your dotfiles
 REPO_DIR="$HOME/dotfiles"
@@ -57,15 +66,16 @@ link_files() {
 # and then open them with your default editor
 edit_file() {
     if [ -z "${EDITOR+x}" ]; then
-        printf "\nThere isn't a default editor (\the EDITOR environment variable isn't set)\n"
+        printf "\nThere isn't a default editor (the EDITOR environment variable isn't set)\n"
     elif [ -z "$(which sk)" ]; then
         echo "skim isn't installed"
     elif [ -z "$(which bat)" ]; then
         echo "bat isn't installed"
+    elif [ -z "$(which fd)" ]; then
+        echo "fd isn't installed"
     else
-        FILE="$(find "$REPO_DIR" -type f |
-            sed '/.git/d;/.png$/d;/.jpg$/d;/.gif$/d;/.svg$/d' |
-            sk --preview="bat {} --color=always")"
+        cd "$REPO_DIR" || (echo "$REPO_DIR is an invalid directory" && exit 1)
+        FILE=$(fd --type f --hidden | sk --preview="bat {} --color=always")
         if [ -n "$FILE" ]; then
             $EDITOR "$FILE"
         fi
@@ -78,13 +88,11 @@ by sirkhancision\n\n"
 
 while true; do
     printf "Options:
-[0] Exit
 [1] Link dotfiles
 [2] Edit file\n"
     read -r OPTION
 
     case $OPTION in
-    0) break ;;
     1) link_files ;;
     2) edit_file ;;
     *) echo "Invalid option" ;;
