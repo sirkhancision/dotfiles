e5044085b317a9c0ae7e1f3e800ad9833594dec8
Jorge Santiago <jsantiago12tone@gmail.com>
2022-12-24 22:29:12 -0300

    Remove unnecessary conditional in xbps and flatpak scripts

--- .config/polybar/scripts/updates-flatpak.sh
+++ .config/polybar/scripts/updates-flatpak.sh
@@ -3,7 +3,5 @@
 updates=$(echo 'n' | flatpak update 2>/dev/null | grep -c '<')
 
 if [ "$updates" -gt 0 ]; then
-	echo "$updates"
-else
-	echo ""
+    echo "$updates"
 fi
--- .config/polybar/scripts/updates-xbps.sh
+++ .config/polybar/scripts/updates-xbps.sh
@@ -3,7 +3,5 @@
 updates=$(xbps-install -Mun 2>/dev/null | wc -l)
 
 if [ -n "$updates" ] && [ "$updates" -gt 0 ]; then
-	echo "$updates"
-else
-	echo ""
+    echo "$updates"
 fi
