a3dd2712142050f721e1103cbb6d85a1e270d163
sirkhancision <jsantiago12tone@gmail.com>
2023-04-10 15:35:17 -0300

    Refactor clock and calendar, and add character limit to modules

--- .config/polybar/scripts/popup-calendar.sh
+++ .config/polybar/scripts/popup-calendar.sh
@@ -4,7 +4,7 @@ BAR_HEIGHT=24  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +"%H:%M")"
+ICON=""
 
 case "$1" in
 --popup)
@@ -36,6 +36,6 @@ case "$1" in
 		--title="yad-calendar" --borders=0 >/dev/null &
 	;;
 *)
-	echo "$DATE"
+	echo "$ICON"
 	;;
 esac
